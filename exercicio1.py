e2cbdb316f9a99fed581c31db7d03616d51001e6
Otto Lima <otto.lima@sou.inteli.edu.br>
2023-08-21 08:52:58 -0300

    esqueci de dar commit

--- exercicio1.py
+++ exercicio1.py
@@ -1,3 +1,5 @@
+#Turma 8 - Otto Bernardo Coutinho Lima
+
 #  Se achar necessario, faça import de outras bibliotecas
 
 
@@ -6,11 +8,10 @@
 
 # Crie a função que será avaliada no exercício aqui
 
-
-
-
-
-
+def conta_palavras_unicas(frase):
+    palavras = frase.lower().split()
+    palavras_unicas = set(palavras)
+    return len(palavras_unicas)
 
 # Teste a sua função aqui (caso ache necessário)
 
--- exercicio2.py
+++ exercicio2.py
@@ -1,3 +1,5 @@
+#Turma 8 - Otto Bernardo Coutinho Lima
+
 #  Se achar necessario, faça import de outras bibliotecas
 
 
@@ -6,11 +8,10 @@
 
 # Crie a função que será avaliada no exercício aqui
 
-
-
-
-
-
+def is_anagram(string1, string2):
+    string1 = string1.lower().replace(" ", "")
+    string2 = string2.lower().replace(" ", "")
+    return sorted(string1) == sorted(string2)
 
 # Teste a sua função aqui (caso ache necessário)
 
